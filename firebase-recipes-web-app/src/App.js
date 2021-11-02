cbf804531c6811be867a75d25b5138b9736ee4c6
jchrisb09 <jbrandt@ggc.edu>
2021-11-02 14:10:57 -0400

    initial commit

---
+++ firebase-recipes-web-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.scss";
+
+import firebase from "./FirebaseConfig";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="title-row">
+        <h2 className="title">Firebase Recipes</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
